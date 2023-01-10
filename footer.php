1bdad08d84abe3e04650a462a03dfc3a89b54c10
Harry Awanda Putra <harrywanda0704@gmail.com>
2023-01-10 10:18:57 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -31,27 +31,6 @@
 <!-- Page level plugins -->
 <script src="vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
 <script src="js/demo/datatables-demo.js"></script>
 </body>
 </html>
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-          </button>
-      </div>
-      <div class="modal-body">Silakan klik tombol logout untuk keluar dari aplikasi ini!.</div>
-      <div class="modal-footer">
-        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a class="btn btn-primary" href="logout.php">Logout</a>
-    </div>
-    </div>
-  </div>
-</div>
