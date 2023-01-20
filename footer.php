5c21ff398bd4bbb72e99c29cacd4218b9b1c38cc
Harry Awanda Putra <harrywanda0704@gmail.com>
2023-01-20 08:28:39 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -23,7 +23,6 @@
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="vendor/bootstrap/js/popper.min.js"></script>
 <!-- Core plugin JavaScript-->
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 <!-- Custom scripts for all pages-->
